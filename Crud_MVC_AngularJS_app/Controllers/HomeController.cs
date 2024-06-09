050e818965fe2d6dd3e0aed263c6bd539517ecf6
kannaujiya2501 <kannaujiyavishal12@gmail.com>
2024-06-09 14:05:26 +0530

    Initial commit

---
+++ Crud_MVC_AngularJS_app/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Crud_MVC_AngularJS_app.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Crud_MVC_AngularJS_app.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Student()
+        {
+            return View();
+        }
+        public string InsertStudentRecord(Student Std)
+        {
+            using(DbEntity db = new DbEntity())
+            {
+                db.Students.Add(Std);
+                db.SaveChanges();
+                return "Student Added Successfully";
+            }
+        }
+        public JsonResult GetAllStudent()
+        {
+            using (DbEntity db = new DbEntity())
+            {
+                DbEntity dbEntity = new DbEntity();
+                var Allrecord = db.Students.ToList();
+                return Json(Allrecord, JsonRequestBehavior.AllowGet);
+               // return "Student Added Successfully";
+            }
+
+        }
+        public string UpdateStudentRecord(Student Std)
+        {
+            using (DbEntity db = new DbEntity())
+            {
+                var record = db.Students.Where(x => x.Id == Std.Id).ToList().FirstOrDefault();
+                record.Name= Std.Name;
+                record.Age= Std.Age;
+                record.Department= Std.Department;
+                db.SaveChanges();
+                return "Student record updated successfully";
+            }
+
+        }
+        public string DeleteStudent(Student Std)
+        {
+            using (DbEntity db = new DbEntity())
+            {
+                var data = db.Students.Where(x => x.Id == Std.Id).ToList().FirstOrDefault();
+                db.Students.Remove(data);
+
+                db.SaveChanges();
+                return "Student record deleted successfully";
+            }
+
+        }
+    }
+}
---
+++ Crud_MVC_AngularJS_app/Scripts/Student.js
@@ -0,0 +1,83 @@
+﻿var app = angular.module("myApp", []);
+app.controller("myCntrl", function ($scope,$http) {
+    $scope.GetAllStudent = function () {
+        $http({
+            method: "get",
+            url: "https://localhost:44321/Home/GetAllStudent"
+        }).then(function (response) {
+            debugger;
+            $scope.student = response.data;
+        }, function () {
+            debugger;
+            alert("Error Occur!");
+
+
+        })
+    };
+    $scope.InsertStudent = function () {
+        var type = document.getElementById("insertStd").getAttribute("value");
+        if (type == "Submit") {
+            $scope.student = {};
+            $scope.student.Name = $scope.SName;
+            $scope.student.Age = $scope.SAge;
+            $scope.student.Department = $scope.SDepartment;
+            $http({
+                method: "post",
+                url: "https://localhost:44321/Home/InsertStudentRecord",
+                datatype: "json",
+                data: $scope.student
+            }).then(function (response) {
+                alert(response.data);
+                $scope.GetAllStudent();
+                $scope.SName = "";
+                $scope.SAge = "";
+                $scope.SDepartment = "";
+            })
+
+        }
+        else {
+            $scope.student = {};
+            $scope.student.id = sessionStorage.getItem("SID");
+            $scope.student.Name = $scope.SName;
+            $scope.student.Age = $scope.SAge;
+            $scope.student.Department = $scope.SDepartment;
+            $http({
+                method: "post",
+                url: "http://localhost:44321/Home/UpdateStudentRecord",
+                datatype: "json"
+                data: JSON.stringify($scope.student)
+            }).then(function (response) {
+                alert(response.data);
+                $scope.GetAllStudent();
+                $scope.SName = "";
+                $scope.SAge = "";
+                $scope.SDepartment = "";
+                document.getElementById("insertStd").getAttribute("value", "Submit");
+            })
+
+        }
+    };
+
+    $scope.UpdateStudent = function (Std) {
+        debugger;
+        sessionStorage.setItem("SID", Std.Id);
+           $scope.SName = $scope.Name,
+            $scope.SAge = $scope.Age,
+            $scope.SDepartment = $scope.Department,
+            document.getElementById("insertStd").getAttribute("value", "Submit");
+    };
+    $scope.DeleteStudent = function (Std) {
+        $http({
+            method: "post",
+            url: "https://localhost:44321/Home/DeleteStudent",
+            datatype: "json"
+             data: JSON.stringify(Std)
+
+        }).then(function (response) {
+            alert(response.data);
+            //alert("deleted successfully!");
+            $scope.GetAllStudent();
+
+        })
+    }
+});
